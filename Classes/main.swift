1f98394cf8cc992b8d9c7cab048e5d625afa39b3
sushmitha <macstudent@126-iMac12.local>
2019-02-13 18:52:25 -0500

    classes and structure

--- Classes/main.swift
+++ Classes/main.swift
@@ -10,3 +10,98 @@ import Foundation
 
 print("Hello, World!")
 
+class Person
+{
+    var pid : Int = 0
+    var fname : String!
+    var lname : String?
+    
+    func setData()
+        
+    {
+        
+        fname = "pritesh"
+       lname = "patel"
+        
+    }
+
+func display()
+{
+    print(pid)
+   //print(fname)
+  // print(lname!)
+
+let l = lname ?? "patel"
+    
+    if let f = fname, let l = lname
+    {
+    
+    let s = fname + " " + l
+        
+        print(s)
+    }
+    
+    print(lname)
+    
+}
+
+}
+
+var p1 = Person()
+
+p1.setData()
+p1.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Student
+{
+    var sid : Int
+    var fname : String
+    var lname : String
+
+
+init ()
+{
+    sid = 0
+    fname = String()
+    lname = String()
+}
+
+init(sid:Int,fname: String,lname : String)
+{
+    self.sid = sid
+    self.fname = fname
+    self.lname = lname
+}
+
+func display()
+{
+    print("student id: \(self.sid)")
+    print("student firstname : \(self.fname)")
+    print("student lastname : \(self.lname)")
+}
+}
+
+var s1 = Student()
+s1.display()
+
+s1.sid = 100
+s1.fname = "pritesh"
+s1.lname = "patel"
+
+s1.display()
+
+let s2 = Student(sid:11, fname: "sushmitha",lname: "varma")
+s1.display()
